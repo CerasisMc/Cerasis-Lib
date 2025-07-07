10ccfa927c1ff0c504646540ef0c219f7e957956
Rodald <poltdavid@icloud.com>
2025-07-07 15:08:21 +0200

    no longer uses the FoodComponent interface

--- cerasislib-items/src/main/java/net/rodald/cerasislib/items/interfaces/Consumable.java
+++ cerasislib-items/src/main/java/net/rodald/cerasislib/items/interfaces/Consumable.java
@@ -4,37 +4,25 @@ import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.components.FoodComponent;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.Map;
-
-public interface Consumable extends FoodComponent, PrepareInterface {
+public interface Consumable extends PrepareInterface {
     void handleConsumption(PlayerItemConsumeEvent event);
-
-    @Override
-    default void setNutrition(int i) {
-
-    }
-
-    @Override
-    default void setSaturation(float v) {
-
-    }
-
-    @Override
-    default void setCanAlwaysEat(boolean b) {
-
-    }
 
     @Override
     default void prepareItem(ItemStack itemStack) {
         ItemMeta itemMeta = itemStack.getItemMeta();
-        itemMeta.setFood(this);
+        FoodComponent foodComponent = itemMeta.getFood();
+
+        foodComponent.setSaturation(getSaturation());
+        foodComponent.setCanAlwaysEat(canAlwaysEat());
+
+        itemMeta.setFood(foodComponent);
         itemStack.setItemMeta(itemMeta);
     }
 
-    @Override
-    @NotNull default Map<String, Object> serialize() {
-        return Map.of();
-    }
+    int getNutrition();
+
+    float getSaturation();
+
+    boolean canAlwaysEat();
 }
